c507347dac93d3e23422b1e4b492291a6a669af3
Ndiritu <ndiritumichael5@gmail.com>
2024-06-11 23:51:27 +0300

    add problem statement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,16 +8,8 @@ import _2_cyclicrotation.oddOccurenceSolution
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-   val array: IntArray = intArrayOf(1,2,3,4,1,2,3,3,3)
-    val shift = oddOccurenceSolution(array)
 
-    println(" the shift is ${shift}")
 
-//    for (i in 1..5) {
-//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//        println("i = $i")
-//    }
+
+
 }
---
+++ src/main/kotlin/_2_cyclicrotation/CyclicRotation.kt
@@ -0,0 +1,78 @@
+package _2_cyclicrotation
+
+
+/**
+ *
+ * [Problem link](https://app.codility.com/programmers/lessons/2-arrays/)
+ *
+ *
+ * An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+ *
+ * The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+ *
+ * Write a function:
+ *
+ * fun solution(A: IntArray, K: Int): IntArray
+ *
+ * that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+ *
+ * For example, given
+ *
+ *     A = [3, 8, 9, 7, 6]
+ *     K = 3
+ * the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+ *
+ *     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+ *     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+ *     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+ * For another example, given
+ *
+ *     A = [0, 0, 0]
+ *     K = 1
+ * the function should return [0, 0, 0]
+ *
+ * Given
+ *
+ *     A = [1, 2, 3, 4]
+ *     K = 4
+ * the function should return [1, 2, 3, 4]
+ *
+ * Assume that:
+ *
+ * N and K are integers within the range [0..100];
+ * each element of array A is an integer within the range [−1,000..1,000].
+ *
+
+ */
+
+fun cyclicRotation(A: IntArray, K: Int): IntArray {
+
+
+    if (A.isEmpty() || K == 0) return A
+    val n = A.size
+
+    // use modulus to avoid unnecesary rotations
+    val actualRotations = K % n
+
+    if (actualRotations == 0) return A
+
+
+    reverse(A, 0, n - actualRotations - 1)
+    reverse(A, n - actualRotations, n - 1)
+    reverse(A, 0, n - 1)
+
+    return A
+}
+
+
+private fun reverse(A: IntArray, start: Int, end: Int) {
+    var left = start
+    var right = end
+    while (left < right) {
+        val temp = A[left]
+        A[left] = A[right]
+        A[right] = temp
+        left++
+        right--
+    }
+}
